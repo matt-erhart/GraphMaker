f40dcb2c23b3a07f583c77ccb5b2c8ee8c9dbd6a
Matt Erhart <mattjerhart@gmail.com>
2017-05-04 16:57:24 -0700

    solved the mystery of the line preview in rxjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ram from 'ramda'
 import NodeDiv from './NodeDiv'
 import TextArea from './TextArea'
 import ZoomContainer from './ZoomContainer'
-import {buttonFromNum, subj} from './constants'
+import {buttonFromNum} from './constants'
 import PropTypes from 'prop-types'
 
 class Figure extends React.Component {
@@ -22,7 +22,8 @@ class Figure extends React.Component {
       links: {},
       selected: [],
       nodes: {}, // {id: {id: ..., x,y,width,height, title?, metadata?, tags?, type? }} //everything will be positioned relative to nodes
-      dragStart: {x: 0, y: 0}
+      dragStart: {x: 0, y: 0},
+      linkStart: {nodeID: ''}
   }
     newCircle = (id, x, y, r) => {
 
@@ -38,7 +39,7 @@ class Figure extends React.Component {
 
     componentWillMount(){
       const {subj} = this.context;
-      const newNode = (click) => ({id: 'node-' + uid.sync(8), x: click.offsetX, y: click.offsetY, text: ''});
+      const newNode = (click) => ({id: 'node-' + uid.sync(8), x: click.offsetX, y: click.offsetY, text: ''}); //@ts-ignore
       // let mouseDown$ = subj.filter(action => action.type === 'LeftDown').map(down => {
       //   let circ = newCircle(down, 1);
       //   this.addCircle(this.state, circ)
@@ -61,18 +62,78 @@ class Figure extends React.Component {
           let dx = moveData.clientX-action.clientX;
           let dy = moveData.clientY-action.clientY;
           console.log(dx,dy)
-          let newNode = {...oldNode, x: x+dx, y: y+dy};
+          let newNode = {...oldNode, x: x+dx, y: y+dy}; //might be faster to mutate
           this.setState({nodes: {...this.state.nodes, [action.id]: newNode}});
           console.log(this.state.nodes[action.id])
       }))
-      Rx.Observable.merge(addNode$,dnd$).subscribe(x => console.log(x));
+      const test = {'link': {} }
+
+      //@ts-ignore
+      const newLink = (source, target) => ({id: 'link-' + uid.sync(8), source, target});
+
+
+
+      let linkClick$     = subj.filter(action => action.type === 'link')
+      let linkOrBGClick$ = Rx.Observable.merge(linkClick$, clickBG$)
+      let startStop$ = linkClick$.take(1).switchMap(clickLink => {
+              return mouseMove$.do(x=>console.log('preview line'))
+              .takeUntil(linkOrBGClick$.take(1).do(x=>console.log('setstate here'))).do(x=>console.log(x))
+      }).repeat()
+      
+      // let startStop$ = Rx.Observable.concat(linkClick$.take(1), 
+      //         mouseMove$.do(x=>console.log('preview line')).takeUntil(linkOrBGClick$.take(1).do(x=>console.log('setstate here')) ) ).repeat().do(x=>console.log(x))
+      
+      // let win$ = linkClick$.switchMap(link => mouseMove$.takeUntil(linkOrBGClick$).combineLatest(linkOrBGClick$))
+    
+      // let makeLink$  = mouseMove$.windowToggle(startStop$.first().do(x=>console.log('1',x)), 
+      // ()=> startStop$.last().do(x=>console.log('2',x))).switch().combineLatest(startStop$);
+
+
+      // const previewLink$ = (clickLink) => {
+      //   return mouseMove$.takeUntil(linkOrBGClick$).do(moveData => {
+      //     let dx = moveData.clientX - clickLink.clientX;
+      //     let dy = moveData.clientY - clickLink.clientY;
+      //     let newLink = { ...this.state.linkStart, x2: x1 + dx, y: y1 + dy }; //might be faster to mutate
+      //     this.setState({ linkStart: newLink })
+      //   }
+      //   )
+      // }
+      // const initializeLink = (clickLink) => {
+      //   let { nodeID, x1, y1 } = this.state.linkStart;
+      //   if (this.state.linkStart.nodeID === '') {
+      //     let { x, y, id } = this.state.nodes[clickLink.id];
+      //     this.setState({ linkStart: { nodeID: id, x1: x, y1: y } })
+      //   }
+      // } 
+      // const addLink = (clickLink) => {
+      //   let { nodeID, x1, y1 } = this.state.linkStart;
+      //   if (nodeID.length > 0 && nodeID !== clickLink.id) {
+      //     let link = newLink(nodeID, clickLink.id)
+      //     this.setState({ links: { ...this.state.links, [link.id]: link } })
+      //     this.setState({ linkStart: { nodeID: '' } })
+      //   }
+      // }
+
+      // let linkCreate$ = linkClick$.take(1).switchMap(clickLink => {
+      //     return previewLink$(clickLink)
+      //  })
+
+        
+
+
+      //   }
+      // )
+
+
+
+      Rx.Observable.merge(addNode$,dnd$,startStop$).subscribe(x => console.log());
 
 
       // let circleRightDown$ = subj.filter(action => action.type === 'circleRightDown').subscribe(x=>console.log('circ',x))
       // let downUpDist$ = (circ) => mouseMove$.takeUntil(mouseUp$).map(move => Math.abs(circ.x-move.offsetX));
       // mouseDown$.mergeMap(circ => downUpDist$(circ).do(distance => {circ.r = distance; this.addCircle(this.state, circ)}))
       
-      // subj.subscribe(x=>console.log(x))
+      //  subj.subscribe(x=>console.log(x))
     
     }
 
@@ -105,11 +166,7 @@ class Figure extends React.Component {
                    <NodeDiv key={node.id} style={{ left: x, top: y }} 
                    >
                      <div>{node.id}</div>
-
-
-
                      <span 
-
                        onMouseDown={e=>{
                          const {clientX, clientY} = e.nativeEvent
                          e.stopPropagation(); 
@@ -117,17 +174,20 @@ class Figure extends React.Component {
                          subj.next({type: 'dragStart', id: node.id, clientX, clientY}); 
                          this.setState({dragStart: {x: node.x, y: node.y}})
                          }} 
-
                        onClick={e=> {e.stopPropagation(); e.preventDefault();}}
-
-                       onMouseUp={e=> {e.stopPropagation(); subj.next({type: 'mouseUp', id: node.id})}}
+                       onMouseUp={e=> {e.stopPropagation(); subj.next({type: 'mouseUp', id: node.id})}}>
+                       drag 
+                       </span> 
+                     <span onClick={e=>{
+                       e.stopPropagation()
+                       const {clientX, clientY} = e.nativeEvent
+                       subj.next({type: 'link', id: node.id, clientX, clientY}); 
                        
-                       >drag </span> 
+                       }}>link </span> 
 
 
 
                      <span onClick={e=>{subj.next({type: 'select', id: node.id})}}>select </span>
-                     <span onClick={e=>{subj.next({type: 'link', id: node.id})}}>link </span> 
                      <span>delete</span>
                      <TextArea rows='1' autoFocus
                        onClick={e => e.stopPropagation()}
